e357301f8abf021666c11085bb30716218d86998
son12039 <sonson12039@gmail.com>
2024-09-27 16:42:11 +0900

    0927

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Main from "./pages/Main";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ index: true, element: <Main /> }],
+  },
+]);
+
+export default router;
